f10f125861c337af0d8023eca4e1108f7c9d785d
Descus <idescusi@gmail.com>
2024-04-26 17:49:39 +0200

    Add Additional Projects

---
+++ Backend.Html/Controllers/HtmlController.cs
@@ -0,0 +1,26 @@
+﻿using RestApiBackend.Contracts;
+
+namespace Backend.Html.Controllers;
+
+public class HtmlController: IEndpointMapper
+{
+    private readonly IEndpointRouteBuilder _routeBuilder;
+
+    public HtmlController(IEndpointRouteBuilder routeBuilder)
+    {
+        _routeBuilder = routeBuilder;
+    }
+
+    public void MapEndpoint()
+    {
+        
+        RouteGroupBuilder htmlGroup = _routeBuilder.MapGroup("html");
+
+        htmlGroup.MapGet("index", () =>
+            {
+                return "SomeText";
+            })
+            .WithName("Html Index")
+            .WithOpenApi();
+    }
+}
