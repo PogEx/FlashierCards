64e8112e088ea8c4838265b85923b894a56da385
Descus <idescusi@gmail.com>
2024-05-23 15:14:53 +0200

    Add folder Changing

--- Backend.RestApi/Contracts/Content/IFolderHandler.cs
+++ Backend.RestApi/Contracts/Content/IFolderHandler.cs
@@ -5,7 +5,7 @@ public interface IFolderHandler
     Task<Guid> CreateFolder(Guid owner, Guid parent, string name);
     Task<Guid> CreateUserRoot(Guid owner);
     Task<bool> DeleteFolder(Guid folder);
-    Task ChangeFolder(string name);
+    Task<bool> ChangeFolder(Guid folder, string? newName = null, Guid? newParent = null);
     Task<IEnumerable<Guid>> GetChildren(Guid folder);
     Task<Guid?> GetParentFolder(Guid folder);
     Task<Guid> GetUserRoot(Guid owner);
