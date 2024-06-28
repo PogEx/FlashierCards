74f4b973d2bcf3c41852a1a66f8a17e6de9f867b
MKD <mert-kaan@live.de>
2024-06-28 12:41:37 +0200

    Add front and backcard fetch

--- Backend.Restart/Components/Pages/CardView.razor.cs
+++ Backend.Restart/Components/Pages/CardView.razor.cs
@@ -1,32 +1,40 @@
+using Backend.Database.Database.Context;
 using Backend.Database.Database.DatabaseModels;
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
 namespace Backend.Restart.Components.Pages;
 
 
 
 public partial class CardView : ComponentBase
 {
-    [Parameter] public Card card { get; set; }
+    [Parameter] public Card frontCard { get; set; }
+
+    [Parameter] public Card backCard { get; set; }
+
     [Parameter] public String CardId { get; set; }
-    
-    
+
+    [Inject] public IDbContextFactory<FlashiercardsContext> DbContextFactory { get; set; }
+
     private bool showFront = true;
-    private Card frontCard = new Card { Title = "Question", Content = "This is the front of the card." };
-    private Card backCard = new Card { Title = "Answer", Content = "This is the back of the card." };
 
     protected override async Task OnInitializedAsync()
     {
-        
+        using (FlashiercardsContext context = await DbContextFactory.CreateDbContextAsync())
+        {
+            Guid cardGuid = Guid.Parse(CardId);
+            frontCard = await context.Cards
+                .Include(c => c.BackCard)
+                .FirstAsync(c => c.CardId == cardGuid);
+
+            backCard = frontCard.BackCard;
+        }
     }
 
     private void ToggleCard()
     {
         showFront = !showFront;
-    }
-
-    public class Card
-    {
-        public string Title { get; set; }
-        public string Content { get; set; }
     }
 }
